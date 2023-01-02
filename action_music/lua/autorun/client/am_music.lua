634194c5d6fa12e164bda9395d5d60b192cde388
relaxtakenotes <91839650+relaxtakenotes@users.noreply.github.com>
2023-01-02 17:01:17 +0500

    more configuration

--- action_music/lua/autorun/client/am_music.lua
+++ action_music/lua/autorun/client/am_music.lua
@@ -1,31 +1,73 @@
 local bosses = {"npc_combinegunship", "npc_hunter", "npc_helicopter", "npc_strider", "a_shit_ton_of_enemies"}
 
-local fade_time = CreateConVar("cl_am_fadetime_mult", "0.5", FCVAR_ARCHIVE, "How fast the music will change.", 0.01, 10)
-local continue_songs = CreateConVar("cl_am_continue_songs", "1", FCVAR_ARCHIVE, "Continue songs where we left off.")
-local reset_last_duration = CreateConVar("cl_am_reshuffle_period", "60", FCVAR_ARCHIVE, "Shuffle some songs in a given period. (0 is disabled)")
-local volume_scale = CreateConVar("cl_am_volume", "0.5", FCVAR_ARCHIVE, "Volume. Epic.")
-local force_type = CreateConVar("cl_am_force_type", "0", FCVAR_ARCHIVE, "If you got some RP scenario where you want only one type of music playing you can change this. 0 - Disabled. 1 - Background. 2 - Battle. 3 - Intense Battle (Boss Battle)")
-local type_song = CreateConVar("cl_am_chat_print", "0", FCVAR_ARCHIVE, "Print music change to chat.")
-
-local last_type = ""
-local timer_name_counter = 0
 local amready = false
 
-local current_channel = nil
+am_current_song = nil
+am_current_channel = nil
 local past_channel = nil
 local channel_locked = false
 
-local last_forget_time = 0
-
 local songs = {}
-local current_song = nil
 local chosen_songs = {}
 chosen_songs["battle"] = NULL
 chosen_songs["battle_intensive"] = NULL
 chosen_songs["background"] = NULL
 
+local last_type = ""
 local is_targeted = nil
+local timer_name_counter = 0
 local by_npc = nil
+local last_forget_time = 0
+
+local fade_time = CreateConVar("cl_am_fadetime_mult", "0.5", FCVAR_ARCHIVE, "How fast the music will change.", 0.01, 10)
+local continue_songs = CreateConVar("cl_am_continue_songs", "1", FCVAR_ARCHIVE, "Continue songs where we left off.")
+local reset_last_duration = CreateConVar("cl_am_reshuffle_period", "60", FCVAR_ARCHIVE, "Shuffle some songs in a given period. (0 is disabled)")
+local force_type = CreateConVar("cl_am_force_type", "0", FCVAR_ARCHIVE, "If you got some RP scenario where you want only one type of music playing you can change this. 0 - Disabled. 1 - Background. 2 - Battle. 3 - Intense Battle (Boss Battle)")
+local type_song = CreateConVar("cl_am_chat_print", "0", FCVAR_ARCHIVE, "Print music change to chat.")
+
+local am_enabled = {}
+am_enabled["battle"] = CreateConVar("cl_am_enabled_background", "1", FCVAR_ARCHIVE, "Obvious!")
+am_enabled["background"] = CreateConVar("cl_am_enabled_battle", "1", FCVAR_ARCHIVE, "Obvious!")
+am_enabled["battle_intensive"] = CreateConVar("cl_am_enabled_battle_intensive", "1", FCVAR_ARCHIVE, "Obvious!")
+
+local am_enabled_global = CreateConVar("cl_am_enabled_global", "1", FCVAR_ARCHIVE, "Obvious!")
+
+local volume_scale = {}
+volume_scale["battle"] = CreateConVar("cl_am_volume_battle", "0.8", FCVAR_ARCHIVE, "Volume. Epic.")
+volume_scale["background"] = CreateConVar("cl_am_volume_background", "0.5", FCVAR_ARCHIVE, "Volume. Epic.")
+volume_scale["battle_intensive"] = CreateConVar("cl_am_volume_battle_intensive", "1", FCVAR_ARCHIVE, "Volume. Epic.")
+
+cvars.AddChangeCallback("cl_am_volume_battle", function(convar_name, value_old, value_new)
+	if tonumber(value_new) < 0.01 then print("Use cl_am_enabled for that.") volume_scale["battle"]:SetFloat(tonumber(value_old)) return end
+    if am_current_song.typee == "battle" then am_current_channel:SetVolume(tonumber(value_new)) end
+end)
+
+cvars.AddChangeCallback("cl_am_volume_background", function(convar_name, value_old, value_new)
+	if tonumber(value_new) < 0.01 then print("Use cl_am_enabled for that.") volume_scale["background"]:SetFloat(tonumber(value_old)) return end
+    if am_current_song.typee == "background" then am_current_channel:SetVolume(tonumber(value_new)) end
+end)
+
+cvars.AddChangeCallback("cl_am_volume_battle_intensive", function(convar_name, value_old, value_new)
+	if tonumber(value_new) < 0.01 then print("Use cl_am_enabled for that.") volume_scale["battle_intensive"]:SetFloat(tonumber(value_old)) return end
+    if am_current_song.typee == "battle_intensive" then am_current_channel:SetVolume(tonumber(value_new)) end
+end)
+
+cvars.AddChangeCallback("cl_am_enabled_background", function(convar_name, value_old, value_new)
+    if am_current_song.typee == "background" and tonumber(value_new) == 0 and IsValid(am_current_channel) then am_current_channel:Stop() end
+end)
+
+cvars.AddChangeCallback("cl_am_enabled_battle", function(convar_name, value_old, value_new)
+    if am_current_song.typee == "battle" and tonumber(value_new) == 0 and IsValid(am_current_channel) then am_current_channel:Stop() end
+end)
+
+cvars.AddChangeCallback("cl_am_enabled_battle_intensive", function(convar_name, value_old, value_new)
+	if am_current_song.typee == "battle_intensive" and tonumber(value_new) == 0 and IsValid(am_current_channel) then am_current_channel:Stop() end
+end)
+
+cvars.AddChangeCallback("cl_am_enabled_global", function(convar_name, value_old, value_new)
+	if tonumber(value_new) == 0 and IsValid(am_current_channel) then am_current_channel:Stop() end
+end)
+
 
 local function parse_am()
 	for i, search_path in ipairs({"sound/am_music/battle/", "sound/am_music/battle_intensive/", "sound/am_music/background/"}) do
@@ -107,6 +149,7 @@ end
 
 
 local function fade_channel(channel, to)
+	if not am_enabled_global:GetBool() then return end
 	local from = channel:GetVolume()
 	local lerp_t = 0
 
@@ -128,6 +171,8 @@ local function fade_channel(channel, to)
 end
 
 local function am_play(typee, delay, force)
+	if not am_enabled_global:GetBool() then return end
+	if not am_enabled[typee]:GetBool() then return end
 	if not amready then return end
 	if channel_locked then return end
 	if force_type:GetInt() == 1 then typee = "background" end
@@ -140,18 +185,17 @@ local function am_play(typee, delay, force)
 		return 
 	end
 
+
 	channel_locked = true
 
 	timer.Simple(delay, function()
 		local song = chosen_songs[typee]
-
 		if not continue_songs:GetBool() then song = songs[typee][math.random(#songs[typee])] end
-
 		if song == nil then return end
 
-		if current_channel != nil then
-			past_channel = current_channel
-			songs[current_song.typee][current_song.index].last_duration = past_channel:GetTime()
+		if IsValid(am_current_channel) then
+			past_channel = am_current_channel
+			songs[am_current_song.typee][am_current_song.index].last_duration = past_channel:GetTime()
 			fade_channel(past_channel, 0)
 		end
 
@@ -169,22 +213,21 @@ local function am_play(typee, delay, force)
 				return 
 			end
 
-			current_channel = station
-			current_song = song
+			am_current_channel = station
+			am_current_song = song
 
-			if continue_songs:GetBool() then current_channel:SetTime(song.last_duration, true) end
+			if continue_songs:GetBool() then am_current_channel:SetTime(song.last_duration, true) end
 
-			current_channel:SetVolume(0)
-			fade_channel(current_channel, 1 * volume_scale:GetFloat())
-
+			am_current_channel:SetVolume(0)
+			fade_channel(am_current_channel, volume_scale[typee]:GetFloat())
 		end)
-
 	end)
 
 	last_type = typee
 end
 
 net.Receive("am_threat_event", function()
+	if not am_enabled_global:GetBool() then return end
 	if not amready then return end
 	is_targeted = net.ReadBool()
 	by_npc = net.ReadString()
@@ -200,20 +243,23 @@ net.Receive("am_threat_event", function()
 end)
 
 hook.Add("Think", "am_think", function()
+	if not am_enabled_global:GetBool() then return end
 	if not amready then return end
-	if current_channel == nil then return end
+	if not am_current_song then return end
 	if engine.TickCount() % 20 != 0 then return end
 
-	local state = current_channel:GetState()
+	local state = 0
+	if IsValid(am_current_channel) then state = am_current_channel:GetState() end
 
-	if (state == 0 or state == 2) then
-		current_song.last_duration = 0
-		chosen_songs[current_song.typee] = songs[current_song.typee][math.random(#songs[current_song.typee])]
-		am_play(current_song.typee, 0, true)
+	if state == 0 then
+		am_current_song.last_duration = 0
+		chosen_songs[am_current_song.typee] = songs[am_current_song.typee][math.random(#songs[am_current_song.typee])]
+		am_play(am_current_song.typee, 0, true)
 	end
 end)
 
 hook.Add("Think", "am_think_forget", function()
+	if not am_enabled_global:GetBool() then return end
 	if not reset_last_duration:GetBool() then return end
 	if CurTime() - last_forget_time > reset_last_duration:GetInt() then
 		last_forget_time = CurTime()
@@ -230,12 +276,12 @@ concommand.Add("cl_am_initialize_songs", function(ply, cmd, args)
 end)
 
 concommand.Add("cl_am_reshuffle", function(ply, cmd, args)
+	if not am_enabled_global:GetBool() then return end
 	if not amready then return end
-	if current_channel == nil then return end
-
-	current_channel:Pause() // channel:stop() makes it null which later fucks up things :\
 
 	shuffle_chosen_songs()
-
 	chat.AddText(Color(50, 50, 255), "[Action Music] ", Color(255, 255, 255), "Reshuffled!")
+
+	if not IsValid(am_current_channel) then return end
+	am_current_channel:Stop()
 end)
